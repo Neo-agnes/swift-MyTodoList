015deba8f6495fa93777d6f552d33ce9aa685f88
Neo-agnes <rmfoi112@gmail.com>
2024-03-29 21:19:19 +0900

    Lv1

---
+++ TodoList/TodoList/MyTodo.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// 데이터 모델을 선언 해야하니까 struct(구조체) 사용
+struct MyTodo {
+    var id: UUID // 요구사항
+    var text: String // 할일에 대한 내용
+    var isCompleted: Bool = false // 스위치를 확인하기 위한 내용, true = 완료 | false = 미완
+}
---
+++ TodoList/TodoList/MyTodoListTableViewCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MyTodoListTableViewCell: UITableViewCell {
+    
+    static let myUser = "MyTodoListTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
